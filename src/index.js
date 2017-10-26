72b4feb9d1d7e4bb299bab9dca8861ecdb3eaa8b
Michael Walker <cmichaelswalker@gmail.com>
2017-10-25 20:41:40 -0700

    finished landing page

--- src/index.js
+++ src/index.js
@@ -58,8 +58,12 @@ function showLoading(bool) {
   $loading.style.display = display;
 }
 
+function link(link) {
+  return `<a href="${link}">${link}</a>`;
+}
+
 function populateProfile(data) {
-  console.log('data', data);
+  console.log('Result Data', data);
 
   // NOTE: reset;
   $profileSection.innerHTML = '';
@@ -67,9 +71,15 @@ function populateProfile(data) {
   const { author, modules } = data;
 
   const userSection = `
-    <div class="user-section">
-      <div class="username">${author.username}</div>
-      <div class="link">${author.link}</div>
+    <div class="section">
+      <div class="user-section">
+        <div class="username sub-title">${author.username}</div>
+        <div class="link">
+          ${link(author.link)}
+        </div>
+        <div>Total Modules: ${modules.length}</div>
+      </div>
+
       ${downloadSection(author.downloads)}
     </div>
   `.trim();
@@ -77,7 +87,7 @@ function populateProfile(data) {
   function downloadSection({lastDay, lastWeek, lastMonth}) {
     return `
       <div class="downloads">
-        Downloads
+        <div class="sub-title">Downloads</div>
         <div class="last-day">Last Day: ${lastDay}</div>
         <div class="last-week">Last Week: ${lastWeek}</div>
         <div class="last-month">Last Month: ${lastMonth}</div>
@@ -85,22 +95,46 @@ function populateProfile(data) {
     `.trim();
   }
 
+function formatDate(ms) {
+  const date = new Date(ms);
+
+  const monthNames = [
+    'January', 'February', 'March',
+    'April', 'May', 'June', 'July',
+    'August', 'September', 'October',
+    'November', 'December'
+  ];
+
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
+  const year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
+
   const mods = modules.map((mod) => {
-    console.log('mod', mod);
+    const modUrl = ((mod.repository || {}).url) || '';
     return `
-      <div class="module">
-        <div class="module-name">${mod.name}</div>
-        <div class="module-version">${mod.version}</div>
-        <div class="module-description">${mod.description}</div>
-        <div class="module-publish-time">${mod.publish_time}</div>
-        <div class="repo">${(mod.repository || {}).url}</div>
-        <div class="license">${mod.license}</div>
+      <div class="module-section section">
+        <div class="module-main">
+          <span class="module-name sub-title">${mod.name}</span>
+          <span class="module-version">${mod.version}</span>
+          <div class="module-description">${mod.description}</div>
+        </div>
         ${downloadSection(mod.downloads)}
+        <div class=module-sub>
+          <div class="module-publish-time">Since: ${formatDate(mod.publish_time)}</div>
+          <div class="repo">${link(modUrl.replace(/git\+/, ''))}</div>
+          <div class="license">License: ${mod.license}</div>
+        </div>
       </div>
     `;
   }).join('');
 
-  const profileInfo = `${userSection}${mods}`;
+  const profileInfo = `
+    ${userSection}
+    ${mods}
+    `.trim();
 
   $profileSection.innerHTML = profileInfo;
 }
